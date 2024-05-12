3da3ac2c5c2ac29e3f7643478fe8f7bc766cac84
beeguriri <kkamboting@gmail.com>
2024-05-12 12:01:12 +0900

    #13 fetch join 사용

--- src/main/java/wendy/study/jpashop/repository/OrderRepositoryCustomImpl.java
+++ src/main/java/wendy/study/jpashop/repository/OrderRepositoryCustomImpl.java
@@ -4,12 +4,14 @@ import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.util.StringUtils;
 import wendy.study.jpashop.model.Order;
+import wendy.study.jpashop.model.QItem;
 import wendy.study.jpashop.params.OrderSearchParam;
 
 import java.util.List;
 
 import static wendy.study.jpashop.model.QMember.member;
 import static wendy.study.jpashop.model.QOrder.order;
+import static wendy.study.jpashop.model.QOrderItem.orderItem;
 
 public class OrderRepositoryCustomImpl extends QuerydslRepositorySupport implements OrderRepositoryCustom {
 
@@ -20,11 +22,13 @@ public class OrderRepositoryCustomImpl extends QuerydslRepositorySupport impleme
     @Override
     public List<Order> searchOrder(OrderSearchParam param) {
 
-        JPQLQuery<Order> query = from(order);
+        JPQLQuery<Order> query = from(order)
+                .leftJoin(order.member, member).fetchJoin()
+                .leftJoin(order.orderItems, orderItem).fetchJoin()
+                .leftJoin(orderItem.item, QItem.item).fetchJoin();
 
         if(StringUtils.hasText(param.getUserName()))
-            query.leftJoin(order.member, member)
-                    .where(member.name.contains(param.getUserName()));
+                    query.where(member.name.contains(param.getUserName()));
 
         if(param.getOrderStatus() != null)
             query.where(order.orderStatus.eq(param.getOrderStatus()));
